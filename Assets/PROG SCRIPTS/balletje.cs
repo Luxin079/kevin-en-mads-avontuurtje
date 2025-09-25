ad32f7105da36f56119545b46c804655088d6670
Luxin079 <hoogeveenmarc@gmail.com>
2025-09-25 10:34:38 +0200

    update

---
+++ Assets/PROG SCRIPTS/balletje.cs
@@ -0,0 +1,65 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    private float elapsedTime = 0f;
+    private GameObject ball;
+
+    void Start()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Color color = RandomColor();
+            Vector3 randPos = RandomPosition(-10f, 10f);
+            CreateBall(color, randPos);
+        }
+    }
+
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime > 1f)
+        {
+            Color color = RandomColor();
+            Vector3 randPos = RandomPosition(-10f, 10f);
+            CreateBall(color, randPos);
+
+            elapsedTime = 0f;
+        }
+
+        if(elapsedTime > 3f)
+        {
+            Destroy(ball);
+        }
+
+     
+    }
+
+    Color RandomColor()
+    {
+        return new Color(Random.value, Random.value, Random.value);
+    }
+
+    Vector3 RandomPosition(float min, float max)
+    {
+        return new Vector3(Random.Range(min, max), Random.Range(min, max), 0f);
+    }
+
+    GameObject CreateBall(Color color, Vector3 position)
+    {
+         ball = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        ball.transform.position = position;
+        ball.GetComponent<Renderer>().material.color = color;
+        ball.AddComponent<Rigidbody>();
+
+        Destroy(ball, 3f);
+
+        return ball;
+
+    }
+
+
+
+}
---
+++ Assets/PROG SCRIPTS/cylinder.cs
@@ -0,0 +1,49 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+public class CylinderSpawner : MonoBehaviour
+{
+    private float elapsedTime = 0f;
+    private GameObject cylinder;
+
+    void Start()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Vector3 randPos = RandomPosition(-10f, 10f);
+            CreateCylinder(color, randPos);
+        }
+    }
+
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime > 1f)
+        {
+            Vector3 randPos = RandomPosition(-10f, 10f);
+            CreateCylinder(color, randPos);
+
+            elapsedTime = 0f;
+        }
+
+        Vector3 RandomPosition(float min, float max)
+        {
+            return new Vector3(Random.Range(min, max), Random.Range(min, max), 0f);
+        }
+
+        GameObject CreateCylinder(Color color, Vector3 position)
+        {
+            cylinder = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            cylinder.transform.position = position;
+            cylinder.GetComponent<Renderer>().material.color = color;
+            cylinder.AddComponent<Rigidbody>();
+
+        }
+
+
+
+
+    }
+
+
