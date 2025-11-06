db1438444c9330a115ea7309bc1f01c4f45b6a5b
Luxin079 <hoogeveenmarc@gmail.com>
2025-11-06 10:02:14 +0100

    yay

--- Assets/PROG SCRIPTS/cylinder.cs
+++ Assets/PROG SCRIPTS/cylinder.cs
@@ -1,49 +1,26 @@
-﻿using Unity.VisualScripting;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CylinderSpawner : MonoBehaviour
+public class cylinder : MonoBehaviour
 {
-    private float elapsedTime = 0f;
-    private GameObject cylinder;
-
-    void Start()
-    {
-        for (int i = 0; i < 100; i++)
-        {
-            Vector3 randPos = RandomPosition(-10f, 10f);
-            CreateCylinder(color, randPos);
-        }
-    }
+    public GameObject towerPrefab;
 
     void Update()
     {
-        elapsedTime += Time.deltaTime;
-
-        if (elapsedTime > 1f)
+        if (Input.GetMouseButtonDown(0))
         {
-            Vector3 randPos = RandomPosition(-10f, 10f);
-            CreateCylinder(color, randPos);
-
-            elapsedTime = 0f;
+            SpawnTower();
         }
-
-        Vector3 RandomPosition(float min, float max)
-        {
-            return new Vector3(Random.Range(min, max), Random.Range(min, max), 0f);
-        }
-
-        GameObject CreateCylinder(Color color, Vector3 position)
-        {
-            cylinder = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            cylinder.transform.position = position;
-            cylinder.GetComponent<Renderer>().material.color = color;
-            cylinder.AddComponent<Rigidbody>();
-
-        }
-
-
-
-
     }
 
+    void SpawnTower()
+    {
 
+        Vector3 randomPos = new Vector3(
+            Random.Range(-5f, 5f),
+            0f,
+            Random.Range(-5f, 5f)
+        );
+
+        Instantiate(towerPrefab, randomPos, Quaternion.identity);
+    }
+}
