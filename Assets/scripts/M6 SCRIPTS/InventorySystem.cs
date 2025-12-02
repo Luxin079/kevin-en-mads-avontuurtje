a96d355918c723f726e4d32db2f2b6a0eb0b3310
Luxin079 <hoogeveenmarc@gmail.com>
2025-12-02 10:16:57 +0100

    prog update

---
+++ Assets/scripts/M6 SCRIPTS/InventorySystem.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySystem : MonoBehaviour
+{
+    private void Start()
+    {
+        _item
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            PickupItem();
+        }
+    }
+}
