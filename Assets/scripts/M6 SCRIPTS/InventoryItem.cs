369e02c341574b3fe5f85e9f1d52e82753bc0f0e
Luxin079 <hoogeveenmarc@gmail.com>
2025-12-09 14:41:38 +0100

    udapte prog op 1

--- Assets/scripts/M6 SCRIPTS/InventoryItem.cs
+++ Assets/scripts/M6 SCRIPTS/InventoryItem.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class InventoryItem : MonoBehaviour
+public abstract class InventoryItem
 {
-    // Start is called before the first frame update
-    void Start()
+    public string Name { get; protected set; }
+    public int Quantity { get; set; }
+
+    public InventoryItem(string name, int quantity = 1)
     {
-        
+        Name = name;
+        Quantity = quantity;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public abstract void Use();
 }
