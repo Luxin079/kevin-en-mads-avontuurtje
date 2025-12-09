fdbcc1aaca35981dd729b1b7af59b7aa6758e7d8
Luxin079 <hoogeveenmarc@gmail.com>
2025-12-09 15:25:02 +0100

    a

---
+++ Assets/scripts/M6 SCRIPTS/Opdracht 3 scripts/ItemStats.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct ItemStats
+{
+    public float damage;
+    public float defense;
+    public float weight;
+
+    // Constructor (optioneel maar handig)
+    public ItemStats(float damage, float defense, float weight)
+    {
+        this.damage = damage;
+        this.defense = defense;
+        this.weight = weight;
+    }
+}
